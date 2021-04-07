4c81f150b6e00fb1a12f56f90095231dcdb535f9
Longk61 <81979929+Longk61@users.noreply.github.com>
2021-04-07 22:17:21 +0700

    Update Quanlynhanvien.cpp
    vấn đề cần làm và hàm nhâp

--- Quanlynhanvien.cpp
+++ Quanlynhanvien.cpp
@@ -1,3 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+struct Employee{
+	char name[40];
+	char province[40];
+	int age;
+};
+typedef  struct Employee NV;
+struct DanhSach
+{
+	NV a[100];
+	int d;
+};
+void Nhap(NV  &nv);
+void NhapN(NV a , int &n);
+void Xuat(NV nv);
+void XuatN(NV a[] , int n);
+void SapXep(NV a[] , int n);
+void XuatFile(NV a[], int n , char fileName );
+void thoat();
+void menu();
+int main(){
+	menu();
+	
+}
 void Nhap(NV &a){
 	fflush(stdin);
 	printf("\n Nhap ten nhan vien : ");
