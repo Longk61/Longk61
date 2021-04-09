9a8d4428ea59ef547cb296bca262e8e056b1d274
Longk61 <81979929+Longk61@users.noreply.github.com>
2021-04-09 12:12:06 +0700

    Update Quanlysinhvien.cpp
    da them ham chen them nhan vien

--- Quanlysinhvien.cpp
+++ Quanlysinhvien.cpp
@@ -9,3 +9,12 @@ struct DanhSach
 	NV a[100];
 	int d;
 };
+void ChenNV(danhsach &ds , NV x, int b){
+
+	fflush(stdin);
+	for(int i = ds.n ; i > b ; i-- ){
+		ds.a[i]=ds.a[i-1];
+	}
+	ds.a[b] = x;
+	ds.n++;
+}
